26639b19d49e32dca8aef7075856e3deb2cd3afc
sulta <fsultan001@citymail.cuny.edu>
2022-02-25 22:18:10 -0500

    FC

---
+++ README.md
@@ -0,0 +1,18 @@
+# upload_to_firebase
+
+### Video Tutorial : https://youtu.be/z_s3q9wda4g
+
+## Information
+
+1. This code is last updated on 10th July 2021
+2. This repo is a flutter porject
+3. This project conts a voice recording app, that allows play the recorded video and upload into firebase storage
+4. Uploaded clip then will be fetched from firebase storage and displayed into the application, where it could be played again.
+
+## Config
+1. Firebase configuration 
+2. Firebase rule setup for storage
+
+## Additional Info
+1. Updated to dart null safety
+2. Tested on physical iOS device only
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:upload_to_firebase/home/view/home_view.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeView(),
+    );
+  }
+}
